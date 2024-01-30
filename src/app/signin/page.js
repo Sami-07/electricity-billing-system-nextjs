f4dfb205dbd046e11b2e96e52d60f565d5a90098
KAshok1 <ashokkumaravel02@gmail.com>
2024-01-30 22:17:32 +0530

    Sign in page

--- src/app/signin/page.js
+++ src/app/signin/page.js
@@ -1,73 +1,151 @@
 "use client";
 
-import { signIn, useSession } from 'next-auth/react';
-import Link from 'next/link';
-import React, { useEffect, useState } from 'react'
-import { Button } from '@/components/ui/button';
-import { Input } from "@/components/ui/input"
-import { useRouter } from 'next/navigation';
+import { signIn, useSession } from "next-auth/react";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+//import { Button } from '@/components/ui/button';
+import { Input } from "@/components/ui/input";
+import { useRouter } from "next/navigation";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+//import Link from '@mui/material/Link';
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
 export default function Signin() {
   const router = useRouter();
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const session = useSession({
-
     // onUnauthenticated() {
     // router.push('/signin')
     // }
-  })
+  });
   const [isSessionExist, setIsSessionExist] = useState(false);
 
   useEffect(() => {
-
-    if (session && session.data && session.data.user && session.data.user.name) {
-
+    if (
+      session &&
+      session.data &&
+      session.data.user &&
+      session.data.user.name
+    ) {
       setIsSessionExist(true);
-    };
-  }, [session])
-  // useEffect(() => {
-  //   if (isSessionExist) {
-  //     router.push('/')
-  //   }
-  // }, [session])
+    }
+  }, [session]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-   
-    const res = await signIn('credentials', {
+    const res = await signIn("credentials", {
       email,
       password,
       callbackUrl: "/",
-      redirect: true
-    }
-    )
+      redirect: true,
+    });
+  };
 
-
-  }
-
-  
   return (
-    <div>
-
-      <form onSubmit={handleSubmit} className=' flex justify-center items-center min-h-screen   '>
-
-        <div className='border-2 rounded-lg flex flex-col w-1/2 lg:w-1/3 gap-7 p-10 py-10' >
-          <p className='text-center text-2xl font-semibold'>Login to TSSPDCL</p>
-
-          
-          <Input placeholder='Enter your email' type='email' value={email} onChange={(e) => setEmail(e.target.value)} />
-          <Input placeholder='Enter your Password' type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
-
-
-          {/* <button type='submit'>Sign IN</button> */}
-          <Button variant={'default'} size={'lg'} >Sign in</Button>
-          <p className=' border-b-2'> </p>
-          <p className='text-center '> OR</p>
-          <Button variant={'outline'} size={'lg'} className='border border-black ' onClick={() =>window.location.href = "/signup"}>Create an Account</Button>
-          <Button variant={'outline'} size={'lg'} className='border border-black ' onClick={() =>window.location.href = "/adminlogin"}>Login as Admin</Button>
-        </div>
-      </form>
-
-    </div>
-  )
+    <>
+      <div>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "contained" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="outlined"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item>
+                  <Link
+                    href="#"
+                    variant="contained"
+                    onClick={() => (window.location.href = "/signup")}
+                  >
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Container>
+      </div>
+    </>
+  );
 }
